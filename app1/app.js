6ed193dea65ab25f6ad043f5a740047e9d044ff9
nomis <milan-b@gmx.net>
2018-03-31 23:20:37 +0200

    extract request method, headers and query-string

--- app1/app.js
+++ app1/app.js
@@ -3,10 +3,18 @@ const url = require("url");
 
 let server = http.createServer((request, response) => {
   const parsedUrl = url.parse(request.url, true);
+  const method = request.method.toLowerCase();
+  const headers = request.headers;
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/|\/$/g, "");
+  const queryStringObject = parsedUrl.query;
+
   response.end("Hello World!");
+
+  console.log("Request received as method", method);
+  console.log("Request received with headers", headers);
   console.log("Request received on path ", trimmedPath);
+  console.log("Request received with query-string", queryStringObject);
 });
 
 server.listen(3000, () => {
