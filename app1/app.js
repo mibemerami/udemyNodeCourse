81c024e7b1d0c786f345f37f8d9866c32d88e4e3
nomis <milan-b@gmx.net>
2018-04-04 23:33:19 +0200

    Send first response using handlers.

--- app1/app.js
+++ app1/app.js
@@ -3,6 +3,7 @@ const url = require("url");
 const StringDecoder = require("string_decoder").StringDecoder;
 
 let server = http.createServer((request, response) => {
+  // Extract data:
   const parsedUrl = url.parse(request.url, true);
   const method = request.method.toLowerCase();
   const headers = request.headers;
@@ -16,11 +17,33 @@ let server = http.createServer((request, response) => {
   });
   request.on("end", () => {
     buffer += stringDecoder.end();
-    response.end("Hello World!");
-    logoutStuff();
+
+    // Responde:
+    let choosenHandler =
+      typeof router[trimmedPath] !== "undefined"
+        ? router[trimmedPath]
+        : handlers.notFound;
+
+    let data = {
+      method,
+      headers,
+      path: trimmedPath,
+      queryStringObject,
+      payload: buffer
+    };
+    choosenHandler(data, function(statusCode, payload) {
+      statusCode = typeof statusCode === "number" ? statusCode : 200;
+      payload = typeof payload === "object" ? payload : {};
+      let payloadString = JSON.stringify(payload);
+      response.setHeader("Content-Type", "application/json");
+      response.writeHead(statusCode);
+      response.end(payloadString);
+    });
+
+    logOutStuff();
   });
 
-  let logoutStuff = () => {
+  let logOutStuff = () => {
     console.log("-----------");
     console.log("Request received as method:", method);
     console.log("Request received with headers:", headers);
@@ -33,3 +56,14 @@ let server = http.createServer((request, response) => {
 server.listen(3000, () => {
   console.log("The Server is listening on port 3000 now.");
 });
+
+let handlers = {};
+handlers.sample = function(data, callback) {
+  callback(406, { name: "Sample handler" });
+};
+handlers.notFound = function(data, callback) {
+  callback(404);
+};
+const router = {
+  sample: handlers.sample
+};
